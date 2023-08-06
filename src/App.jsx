76ce36d59632560dd5e22ac6b701181d6fdc99e5
Belen Torres <belentorress@gmail.com>
2023-08-06 18:07:22 -0300

    Modificación en estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,32 @@
-
 import logoList from "./imagenes/logo.png"
 import { ListaDeTareas } from "./componentes/ListaDeTareas/ListaDeTareas"
 import { Row, Col } from "react-bootstrap"
-
 import './App.css'
 
 function App() {
 
   return (
-    <div className="container-fluid text-center h-100">
-      <Row className="d-flex justify-content-center align-items-center aplicacion-tareas" >
-        <Col xs={12} sm={12} md={6} lg={4} className="text-center">
-          <div className="d-flex flex-row list-logo-contenedor ms-4 my-4">
+    <div className="container-fluid ">
+      <Row className="d-flex justify-content-center aplicacion-tareas" >
+        <Col xs={12} sm={12} md={6} lg={4} className="my-4 columnas-aplicacion-tareas">
+          <div className="d-flex flex-row justify-content-center align-items-center list-logo-contenedor mt-5">
             <img
               src={logoList}
-              className='list-logo' />
-            <h1 className="mt-1">My To Do List</h1>
+              className='list-logo me-2' />
+            <h1 className="ms-1" >My To Do List</h1>
           </div>
-
-          <div className="tareas-lista-principal p-3">
-            <h2>Mis Tareas</h2>
-            <ListaDeTareas />
-          </div>
-
         </Col>
-
       </Row>
 
+      <Row className="d-flex justify-content-center">
+        <Col xs={12} sm={12} md={6} lg={4}>
+          <div className="tareas-lista-principal p-3">
+            <h3>Mis Tareas</h3>
+            <ListaDeTareas/>
+          </div>
+        </Col>
+      </Row>
     </div>
-    
-      
-
-    
-
-    
   )
 }
 
